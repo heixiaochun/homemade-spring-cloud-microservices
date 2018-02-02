fdd11fafcca9653bfb99280b22067c20ffef2c71
xiaoping <lurkerping@gmail.com>
2018-02-02 20:09:22 +0800

    Eureka Service Initial Commit

---
+++ homemade-eureka-service/src/main/java/com/xplmc/learning/eureka/EurekaServiceApplication.java
@@ -0,0 +1,20 @@
+package com.xplmc.learning.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * homemade eureka server example
+ *
+ * @author luke
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class);
+    }
+
+}
