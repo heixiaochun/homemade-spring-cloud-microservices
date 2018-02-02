fdf309bee20bdbb23f3816823f6ea850bcc3cc94
xiaoping <lurkerping@gmail.com>
2018-02-02 22:25:12 +0800

    Whitelist Client example

---
+++ homemade-whitelist-client/src/main/java/com/xplmc/learning/whitelist/client/common/WhitelistClientConstants.java
@@ -0,0 +1,12 @@
+package com.xplmc.learning.whitelist.client.common;
+
+/**
+ * some constants
+ *
+ * @author luke
+ */
+public final class WhitelistClientConstants {
+
+    public static final String WHITELIST_SERVICE_NAME = "homemade-whitelist-service";
+
+}
--- homemade-whitelist-client/src/main/java/com/xplmc/learning/whitelist/client/component/WhitelistClientRunner.java
+++ homemade-whitelist-client/src/main/java/com/xplmc/learning/whitelist/client/component/WhitelistClientRunner.java
@@ -1,8 +1,12 @@
 package com.xplmc.learning.whitelist.client.component;
 
+import com.xplmc.learning.whitelist.client.common.WhitelistClientConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,9 +19,28 @@ public class WhitelistClientRunner implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(WhitelistClientRunner.class);
 
-    @Override
-    public void run(String... strings) throws Exception {
-        logger.info("here i am");
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    public WhitelistClientRunner(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("all registered services list, {}", discoveryClient.getServices());
+
+        //list all whitelist services
+        String serviceId = WhitelistClientConstants.WHITELIST_SERVICE_NAME;
+        logger.info("registered instance of {} in eureka service registry", serviceId);
+        discoveryClient.getInstances(serviceId).forEach(this::printServiceInstance);
+
+    }
+
+    /**
+     * just print the service instance
+     */
+    private void printServiceInstance(ServiceInstance si) {
+        logger.info("serviceId:{}, uri:{}", si.getServiceId(), si.getUri());
+    }
 }
