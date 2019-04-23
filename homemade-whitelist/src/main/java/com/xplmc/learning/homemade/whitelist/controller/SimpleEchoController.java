a532437a3ce3772be8e5d4ebc387876358f39fbc
xiaoping <lurkerping@gmail.com>
2019-04-23 22:22:20 +0800

    support get and post etc.

--- homemade-whitelist/src/main/java/com/xplmc/learning/homemade/whitelist/controller/SimpleEchoController.java
+++ homemade-whitelist/src/main/java/com/xplmc/learning/homemade/whitelist/controller/SimpleEchoController.java
@@ -5,11 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -30,19 +28,33 @@ public class SimpleEchoController {
         this.mockResponseProperties = mockResponseProperties;
     }
 
-    @GetMapping(("/simple/echo/{text}"))
-    public Map<String, String> echo(@PathVariable String text) {
+    @RequestMapping(("/simple/echo/{text}"))
+    public Map<String, String> echo(@PathVariable String text,
+                                    @RequestParam(required = false) Long sleepTime,
+                                    @RequestBody(required = false) HashMap<String, String> params) {
+        // using blow to throw some error in random
+        mockOperation();
+        // sleep for a certain time
+        sleepForAWhile(sleepTime);
+        params.put("text", text);
+        return params;
+    }
+
+    private void mockOperation() {
         logger.info("mockResponseProperties: {}", mockResponseProperties);
         if (mockResponseProperties.isRandomInternalError() && mockResponseProperties.getInternalErrorChance() > Math.random()) {
             throw new RuntimeException("not very luck!");
         }
-        try {
-            Thread.sleep((long) (Math.random() * 10L));
-        } catch (InterruptedException e) {
-            //ignore
+    }
+
+    private void sleepForAWhile(Long sleepTime) {
+        if (sleepTime != null) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                //ignore
+            }
         }
-        logger.debug("echo: {}", text);
-        return Collections.singletonMap("text", text);
     }
 
 }
